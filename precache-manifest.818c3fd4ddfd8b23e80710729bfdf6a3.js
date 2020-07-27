88ac052771dd2488c57d1d2308ad3614a256ad5d
Minhwa Song <minhwa@Minhwaui-MacBookPro.local>
2020-07-27 15:27:06 +0900

    Updates

---
+++ precache-manifest.818c3fd4ddfd8b23e80710729bfdf6a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdce1fe5f685bd3e9635855031f64115",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "607c2e3ec7c7b6e37a9e",
+    "url": "/movie_app_2020/static/css/main.3e122c99.chunk.css"
+  },
+  {
+    "revision": "b4ad6d5796d4c9c3f3c1",
+    "url": "/movie_app_2020/static/js/2.3507762e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.3507762e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "607c2e3ec7c7b6e37a9e",
+    "url": "/movie_app_2020/static/js/main.ba13591b.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
